b173ed4f37a496c9582a59118d951287758bc614
Kanyesigye2002 <kanyeallan@gmail.com>
2021-11-27 16:40:36 +0300

    buildspec file

--- src/main/java/com/vumah/vumahapis/VumahApisApplication.java
+++ src/main/java/com/vumah/vumahapis/VumahApisApplication.java
@@ -14,7 +14,7 @@ public class VumahApisApplication {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("*")
+                registry.addMapping("/**")
                         .allowedMethods("*")
                         .allowedOrigins("*");
             }
