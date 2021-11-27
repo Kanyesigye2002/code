db59d1e5b3977bbb5268a67bd8ec955e8f8c6cdd
Kanyesigye2002 <kanyeallan@gmail.com>
2021-11-27 16:32:14 +0300

    buildspec file

--- src/main/java/com/vumah/vumahapis/VumahApisApplication.java
+++ src/main/java/com/vumah/vumahapis/VumahApisApplication.java
@@ -3,9 +3,23 @@ package com.vumah.vumahapis;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class VumahApisApplication {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("*")
+                        .allowedMethods("*")
+                        .allowedOrigins("*");
+            }
+        };
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(VumahApisApplication.class, args);
